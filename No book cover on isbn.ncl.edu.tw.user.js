3a7bd3bab8103a7f924c0a6b3030c48f2ddba2f4
Wei-Li Tang <alextwl@gmail.com>
2025-06-23 19:32:08 +0800

    remove favicon

--- No book cover on isbn.ncl.edu.tw.user.js
+++ No book cover on isbn.ncl.edu.tw.user.js
@@ -7,7 +7,6 @@
 // @description:zh-TW 隱藏國家圖書館全國新書資訊網書目資料庫查詢結果當中的書籍封面
 // @author       alextwl
 // @match        https://isbn.ncl.edu.tw/NEW_ISBNNet/*Results*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=https%3A%2F%2Fisbn.ncl.edu.tw
 // @license      MIT
 // @grant        none
 // ==/UserScript==
